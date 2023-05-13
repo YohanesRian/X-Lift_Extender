2be86a2b45fc1fe6fda52e35282fa8a548a5fd1e
YohanesRian <91827442+YohanesRian@users.noreply.github.com>
2023-05-13 21:31:13 +0700

    Adding Empty Template Database file

---
+++ x_lift.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Bulan Mei 2023 pada 16.29
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `x_lift`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `card`
+--
+
+CREATE TABLE `card` (
+  `CardID` varchar(20) NOT NULL,
+  `UserID` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `role`
+--
+
+CREATE TABLE `role` (
+  `UserID` varchar(20) NOT NULL,
+  `Role` varchar(10) NOT NULL CHECK (`Role` = 'staff' or `Role` = 'customer')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `room`
+--
+
+CREATE TABLE `room` (
+  `RoomID` int(5) NOT NULL,
+  `RoomName` varchar(60) NOT NULL,
+  `Floor` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `schedule_access`
+--
+
+CREATE TABLE `schedule_access` (
+  `UserID` varchar(20) NOT NULL,
+  `AccessIn` datetime NOT NULL,
+  `AccessOut` datetime NOT NULL,
+  `RoomID` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `card`
+--
+ALTER TABLE `card`
+  ADD PRIMARY KEY (`CardID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indeks untuk tabel `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indeks untuk tabel `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`RoomID`);
+
+--
+-- Indeks untuk tabel `schedule_access`
+--
+ALTER TABLE `schedule_access`
+  ADD PRIMARY KEY (`UserID`,`AccessIn`,`AccessOut`,`RoomID`),
+  ADD KEY `RoomID` (`RoomID`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `card`
+--
+ALTER TABLE `card`
+  ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `role` (`UserID`);
+
+--
+-- Ketidakleluasaan untuk tabel `schedule_access`
+--
+ALTER TABLE `schedule_access`
+  ADD CONSTRAINT `schedule_access_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `role` (`UserID`),
+  ADD CONSTRAINT `schedule_access_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
