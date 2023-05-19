a942c64213cd930efea76b584896ae766ee2dccb
YohanesRian <91827442+YohanesRian@users.noreply.github.com>
2023-05-19 18:48:02 +0700

    Floor to floor

--- PHP Request Data/Get_Floor.php
+++ PHP Request Data/Get_Floor.php
@@ -9,7 +9,7 @@
 
     if(mysqli_num_rows($result) > 0){
         while ($data = mysqli_fetch_array($result)) {
-            echo $data['Floor'];
+            echo $data['floor'];
         }
     }
     else{
