96d144f8344a85f1473781d5c08bfb0867405b23
YohanesRian <91827442+YohanesRian@users.noreply.github.com>
2023-05-22 03:15:44 +0700

    Update query

--- PHP Request Data/Get_Access.php
+++ PHP Request Data/Get_Access.php
@@ -13,7 +13,8 @@
                 echo $data['Role'];
             }
             else{
-                $query = "SELECT DISTINCT room.Floor FROM schedule_access JOIN card ON (schedule_access.UserID = card.UserID) JOIN room ON (schedule_access.RoomID = room.RoomID) WHERE ((schedule_access.AccessIn <= CURRENT_TIMESTAMP AND schedule_access.AccessOut >= CURRENT_TIMESTAMP) OR (schedule_access.AccessIn - INTERVAL 1 HOUR >= CURRENT_TIMESTAMP)) AND card.CardID = '$card_id' LIMIT 1;";
+                
+                $query = "SELECT DISTINCT room.Floor FROM schedule_access JOIN card ON (schedule_access.UserID = card.UserID) JOIN room ON (schedule_access.RoomID = room.RoomID) WHERE (schedule_access.AccessIn - INTERVAL 1 HOUR <= CURRENT_TIMESTAMP AND schedule_access.AccessOut >= CURRENT_TIMESTAMP) AND card.CardID = '$card_id' LIMIT 1;";
     
                 $result = mysqli_query($connect, $query);
                 if(mysqli_num_rows($result) > 0){
